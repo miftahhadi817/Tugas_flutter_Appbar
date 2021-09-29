de842e9fcb5287d6fa26e9d03b69ed14d621f95c
miftahhadi817 <81151721+miftahhadi817@users.noreply.github.com>
2021-09-29 14:33:07 +0800

    Tugas

---
+++ android/app/src/main/kotlin/com/example/flutter_hadi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_hadi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          leading: Icon(Icons.menu),
+          title: Text('Hello Appbar'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.black),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert, color: Colors.black),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Center(
+          child: Text('Tugas_19710036 Miftah Hadi'),
+        ),
+      ),
+    );
+  }
+}
